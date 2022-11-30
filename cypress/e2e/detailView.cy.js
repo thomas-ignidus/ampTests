9bea6ac513d969b7954fc5bdab88687c3c6b4010
thomas-ignidus <37197885+thomas-ignidus@users.noreply.github.com>
2022-11-30 13:44:12 -0800

    AD-7307 updated title

--- cypress/e2e/detailView.cy.js
+++ cypress/e2e/detailView.cy.js
@@ -1,11 +1,10 @@
 describe('detailView.cy.js', () => {
-  it('login', () => {
+  it('open detail view', () => {
     cy.login('https://suttongl.isceng.net', 'thomas@sisinsure.com', 'f')
 
     cy.get('div.advancedSearch').click()
     cy.get('select[name="status_id"]').select('Quote Approved')
     cy.get('button[type="submit"]').click()
-    // cy.pause()
     cy.get("tr.itemRow.searchRow td").first().then(function($itemId) {
       cy.visit('https://suttongl.isceng.net/amp/detail/view/' + $itemId.text())
     })
