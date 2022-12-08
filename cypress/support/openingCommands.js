070bc7f42e9b4e7cafc11041b23524a0f0653dd6
thomas-ignidus <37197885+thomas-ignidus@users.noreply.github.com>
2022-12-08 11:49:43 -0800

    AD-7307 updated audit cron with new sutton product ids

--- cypress/support/openingCommands.js
+++ cypress/support/openingCommands.js
@@ -1,7 +1,7 @@
 
 Cypress.Commands.add('login', (site, username, password) => {
     cy.visit(site + '/amp/login')
-    cy.get('#username').type(username)
+    cy.get('#username').type(username).debug()
     cy.get('#password').type(password)
     cy.get('button[type="submit"]').click()
 })
